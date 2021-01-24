497ad7f99c6d6b494849ad590c243bece85af2f3
Francolino Mattos <francolinomattos@gmail.com>
2021-01-23 19:43:10 -0500

    Confirmed API interface working

---
+++ API/generate.rb
@@ -0,0 +1,62 @@
+require 'excon'
+require 'json'
+require_relative "methods.rb"
+
+# rXg production environments should ALWAYS have a valid ssl certificate
+# If no valid ssl certificate for device for testing purposes, uncomment line below
+Excon.defaults[:ssl_verify_peer] = false
+
+DEVICE_ADDRESS = ""
+if DEVICE_ADDRESS.empty?
+  puts "Enter device address:"
+  DEVICE_ADDRESS = STDIN.gets.chomp!
+end
+
+API_KEY = ""
+if API_KEY.empty?
+  puts "Enter device address:"
+  API_KEY = STDIN.gets.chomp!
+end
+
+# Checks if no arguments passed, then runs default config
+if ARGV.empty?
+  puts "predefined config."
+  exit!
+
+# Validates that every device has a number of instances to create
+elsif ARGV.length.odd?
+  puts "Valid Format: Device count."
+  puts "You may create as many devices as you wish in one line."
+  exit
+end
+
+# instantiating  the API class
+devapi = Rxg_API.new(DEVICE_ADDRESS,API_KEY)
+
+# Stores created devices
+generated_devices = []
+# Check if device is valid option and creates device
+ARGV.each_index do |i|
+  # If it is a number, skip 
+  next if ARGV[i].to_i != 0
+  # If next device is not a number or negative number, skip
+  next if ARGV[i + 1].to_i <= 0
+
+  case ARGV[i].downcase
+  when "switch"
+    #devapi.create_switch(ARGV[i + 1].to_i)
+  when "account"
+    #puts "account"
+  else
+    puts "Automatic generation for #{ARGV[i]} is not yet supported."
+    next
+  end
+  generated_devices.push({:name => ARGV[i], :count =>  ARGV[i+1]})
+end
+
+generated_devices.each_index do |i|
+  puts "Created #{generated_devices[i][:name]} count: #{generated_devices[i][:count]}"
+end
+
+
+puts Excon.get("https://192.168.80.34/admin/scaffolds/infrastructure_devices/index.json?api_key=tdlRrSgyPEHoADDTAGhNIfCosQyMBkq3uauAKDu3ox6mpwLVXRl6TlTLqNUVPIOm6THiHfWJiAwBiHyke_y2tQ").body
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# rXg_generators
+# Ruby - rXg device generator
+
+Ruby interface for generating devices to be emulated inside the rXg interface.
+
+The tools folder includes a generator to be installed and run inside the rXg shell.
+
+The API folder utilizes the RESTful API of the rXg and runs remotely. Prior to using the API you must got to System > Admin, select your user and click Show, retreive your API key.
+
+## Installation
+
+`sh gem install excon `
